55cdf836cee4483306ebabbb7531d7c7d96ef07f
Maksim2001-web <89614701484@mail.ru>
2024-11-11 11:06:00 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,73 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+# Создание клавиатуры
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button_calculate = KeyboardButton("Рассчитать")
+button_info = KeyboardButton("Информация")
+keyboard.add(button_calculate, button_info)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=keyboard)
+
+
+@dp.message_handler(lambda message: message.text == 'Рассчитать')
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост в сантиметрах:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес в килограммах:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+    await message.answer(f"Ваша норма калорий: {calories:.2f} ккал.")
+
+    await state.finish()
+
+
+@dp.message_handler(lambda message: True)
+async def all_messages(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
